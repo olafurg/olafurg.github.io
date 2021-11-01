4dc6790e81516ad18dc9a1dae18314a9bd936b40
Ólafur Guðmundsson <olafur.g@gmail.com>
2021-11-01 23:42:54 +0000

    Remove SPF record, not used anymore

--- terraform/cf_dns.tf
+++ terraform/cf_dns.tf
@@ -13,12 +13,3 @@ resource "cloudflare_record" "olafurgcom-cf-pages-www" {
   value   = "olafurg-github-io.pages.dev"
   proxied = true
 }
-
-resource "cloudflare_record" "terraform_managed_resource_da78c05dabcd8cb081f0d7643bc3c275" {
-  name    = "olafurg.com"
-  proxied = false
-  ttl     = 1
-  type    = "TXT"
-  value   = "v=spf1 include:spf.messagingengine.com"
-  zone_id = var.zone_id
-}
