60ebf7737e6341e00ef6b4ef919de2014d9e5079
Olafur Gudmundsson <olafur.g@gmail.com>
2022-02-03 20:43:49 +0000

    Add restrictive SPF, DKIM, DMARC to prevent spoofing Since I don't have email set up this will tell email servers to reject all emails from my domain.

--- terraform/cf_dns.tf
+++ terraform/cf_dns.tf
@@ -13,3 +13,28 @@ resource "cloudflare_record" "olafurgcom-cf-pages-www" {
   value   = "olafurg-github-io.pages.dev"
   proxied = true
 }
+
+// DKIM, DMARC, SPF to drop all email (since I don't have email configured)
+resource "cloudflare_record" "spf" {
+  zone_id = var.zone_id
+  type = "TXT"
+  name = "olafurg.com"
+  value = "v=spf1 -all"
+  proxied = false
+}
+
+resource "cloudflare_record" "dkim" {
+  zone_id = var.zone_id
+  type = "TXT"
+  name = "*._domainkey"
+  value = "v=DKIM1; p="
+  proxied = false
+}
+
+resource "cloudflare_record" "dmarc" {
+  zone_id = var.zone_id
+  type = "TXT"
+  name = "_dmarc"
+  value = "v=DMARC1; p=reject; sp=reject; adkim=s; aspf=s;"
+  proxied = false
+}
