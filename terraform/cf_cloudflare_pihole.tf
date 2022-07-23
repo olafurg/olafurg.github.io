8e5376efee2d934f51644307d2cb627591144954
Olafur Gudmundsson <olafur.g@gmail.com>
2022-07-23 22:59:53 +0000

    Delete cf_cloudflare_pihole.tf

--- terraform/cf_cloudflare_pihole.tf
+++
@@ -1,7 +0,0 @@
-resource "cloudflare_record" "pihole" {
-  zone_id = var.zone_id
-  type    = "A"
-  name    = "pihole"
-  value   = "153.92.153.150"
-  proxied = true // hides the IP address destination
-}
