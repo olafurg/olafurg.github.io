9c4b9c68f7fb5047990a7105b6c5fc76a19254d4
Ólafur Guðmundsson <olafur.g@gmail.com>
2022-07-24 01:29:10 +0000

    Change redirect approach

--- terraform/cf_pihole.tf
+++ terraform/cf_pihole.tf
@@ -1,16 +1,7 @@
-// DNS
-resource "cloudflare_record" "ph" {
-  zone_id = var.zone_id
-  type    = "A"
-  name    = "ph"
-  value   = "1.2.3.4"
-  proxied = true
-}
-
 // Page rule to redirect
 resource "cloudflare_page_rule" "pihole-redirect" {
   zone_id = var.zone_id
-  target  = "ph.olafurg.com/*"
+  target  = "pihole.olafurg.com"
 
   lifecycle { // Ignore because Cloudflare
     ignore_changes = [priority]
