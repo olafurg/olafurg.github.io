52883f56821a924dfef9d816e9d181b17f2989bc
Ólafur Guðmundsson <olafur.g@gmail.com>
2022-07-24 00:18:39 +0000

    Test

--- terraform/cf_pihole.tf
+++ terraform/cf_pihole.tf
@@ -1,7 +1,16 @@
+// DNS
+resource "cloudflare_record" "ph" {
+  zone_id = var.zone_id
+  type    = "CNAME"
+  name    = "ph"
+  value   = "1.2.3.4"
+  proxied = true
+}
+
 // Page rule to redirect
 resource "cloudflare_page_rule" "pihole-redirect" {
   zone_id = var.zone_id
-  target  = "pihole.olafurg.com/*"
+  target  = "ph.olafurg.com/*"
 
   lifecycle { // Ignore because Cloudflare
     ignore_changes = [priority]
