a1f8604c00652ded01692cfe2596b77d3f2305ee
Ólafur Guðmundsson <olafur.g@gmail.com>
2022-11-28 15:05:29 +0000

    Add redirect boilerplate and 'my' redirect.

---
+++ terraform/cf_redirects.tf
@@ -0,0 +1,40 @@
+// List of simple redirects
+
+// Add subdomains of ccpgames.com and their redirect destinations here
+locals {
+  redirects = {
+    my = "https://www.tesla.com/ownersmanual/modely/is_is/"
+  }
+}
+
+// REDIRECT DEFINITIONS BELOW - IGNORE IF ONLY ADDING/EDITING/REMOVING REDIRECT
+
+// Cloudflare record
+resource "cloudflare_record" "redirect-record" {
+  for_each = local.redirects
+
+  zone_id = var.cloudflare_zone_id
+  type    = "A"
+  name    = each.key
+  value   = "1.2.3.4" // Dummy, won't be used due to the redirect page rule
+  proxied = true
+}
+
+// Page rule to redirect
+resource "cloudflare_page_rule" "redirect-rule" {
+  for_each = local.redirects
+
+  zone_id = var.cloudflare_zone_id
+  target  = format("%s.%s%s", each.key, var.ccpgames_domain, "/*")
+
+  lifecycle { // Ignore because Cloudflare
+    ignore_changes = [priority]
+  }
+
+  actions {
+    forwarding_url {
+      url         = each.value
+      status_code = "301"
+    }
+  }
+}
