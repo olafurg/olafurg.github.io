6eeab639bd86d76d43e65f61efc26035beea4919
Ólafur Guðmundsson <olafur.g@gmail.com>
2023-03-05 15:09:42 +0000

    Needs DNS record for subdomain

--- terraform/test_redirects.tf
+++ terraform/test_redirects.tf
@@ -4,6 +4,16 @@
 # If wiki.olafurg.com/display/*
 # Don't do anything
 
+// Cloudflare record
+resource "cloudflare_record" "wiki" {
+
+  zone_id = var.zone_id
+  type    = "CNAME"
+  name    = "wiki"
+  value   = "pihole" // Dummy, won't be used due to the 
+  proxied = true
+}
+
 resource "cloudflare_ruleset" "redirect_from_list_example" {
   zone_id     = var.zone_id
   name        = "redirects"
