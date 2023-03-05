956e9512eb90b23b15f5307754ada79fc4d3e9f2
Ólafur Guðmundsson <olafur.g@gmail.com>
2023-03-05 00:54:32 +0000

    Fix typo

---
+++ terraform/test_redirects.tf
@@ -0,0 +1,28 @@
+# If wiki.olafurg.com (and nothing else)
+# Redirect to somewhere else
+
+# If wiki.olafurg.com/display/*
+# Don't do anything
+
+resource "cloudflare_ruleset" "redirect_from_list_example" {
+  zone_id = var.zone_id
+  name        = "redirects"
+  description = "Redirect ruleset"
+  kind        = "root"
+  phase       = "http_request_redirect"
+
+  rules {
+    action = "redirect"
+    action_parameters {
+        from_value {
+          status_code = 301
+          target_url {
+            value = "visir.is"
+          }
+        }
+      }
+    }
+    expression  = "(http.request.uri eq \"\" and http.host eq \"wiki.olafurg.com\")"
+    description = "Apply redirects"
+    enabled     = true
+  }
