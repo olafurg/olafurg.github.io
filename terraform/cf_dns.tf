25deb29c617311d3b0fe69b6ccd35fb44b0a3279
Olafur Gudmundsson <1510904+olafurg@users.noreply.github.com>
2026-01-01 10:43:28 +0000

    Email routing DKIM, DMARC

--- terraform/cf_dns.tf
+++ terraform/cf_dns.tf
@@ -21,7 +21,7 @@ resource "cloudflare_dns_record" "spf" {
   zone_id = var.zone_id
   type    = "TXT"
   name    = "olafurg.com"
-  content = "v=spf1 include:_spf.mx.cloudflare.net ~all"
+  content = "v=spf1 include:_spf.mx.cloudflare.net -all"
   proxied = false
   ttl     = 1
 }
@@ -35,11 +35,20 @@ resource "cloudflare_dns_record" "dkim" {
   ttl     = 1
 }
 
+resource "cloudflare_dns_record" "dkim_cf" {
+  zone_id = var.zone_id
+  type    = "TXT"
+  name    = "cf2024-1._domainkey"
+  content = "v=DKIM1; h=sha256; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAiweykoi+o48IOGuP7GR3X0MOExCUDY/BCRHoWBnh3rChl7WhdyCxW3jgq1daEjPPqoi7sJvdg5hEQVsgVRQP4DcnQDVjGMbASQtrY4WmB1VebF+RPJB2ECPsEDTpeiI5ZyUAwJaVX7r6bznU67g7LvFq35yIo4sdlmtZGV+i0H4cpYH9+3JJ78km4KXwaf9xUJCWF6nxeD+qG6Fyruw1Qlbds2r85U9dkNDVAS3gioCvELryh1TxKGiVTkg4wqHTyHfWsp7KD3WQHYJn0RyfJJu6YEmL77zonn7p2SRMvTMP3ZEXibnC9gz3nnhR6wcYL8Q7zXypKTMD58bTixDSJwIDAQAB"
+  proxied = false
+  ttl     = 1
+}
+
 resource "cloudflare_dns_record" "dmarc" {
   zone_id = var.zone_id
   type    = "TXT"
   name    = "_dmarc"
-  content = "v=DMARC1; p=reject; sp=reject; adkim=s; aspf=s;"
+  content = "v=DMARC1; p=reject; sp=reject; adkim=s; aspf=s; rua=mailto:e366d8b47d304fb1a63cca9477a28994@dmarc-reports.cloudflare.net;"
   proxied = false
   ttl     = 1
 }
